afe6c59fa2128abb9cc7f6397a17fa122974d7b4
Raphael Trzpit <rtrzpit@gmail.com>
2017-09-09 13:04:16 +0200

    init

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
+];
